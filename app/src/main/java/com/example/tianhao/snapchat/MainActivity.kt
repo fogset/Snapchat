0989b4c58c224ba8c0a50b1646863ca25210be73
Tianhao <zhangth1990@hotmail.com>
2018-06-18 23:51:08 -0600

    setting up initial variables

--- app/src/main/java/com/example/tianhao/snapchat/MainActivity.kt
+++ app/src/main/java/com/example/tianhao/snapchat/MainActivity.kt
@@ -2,11 +2,21 @@ package com.example.tianhao.snapchat
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
-
+    var emailEditText: EditText? = null
+    var passwordEditText: EditText? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        emailEditText = findViewById(R.id.emailEditText)
+        passwordEditText = findViewById(R.id.passwordEditText)
+    }
+
+    fun goClicked(view: View){
+
     }
 }
