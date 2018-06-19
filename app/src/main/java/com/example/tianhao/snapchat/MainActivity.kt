a3c6bca5667ca157c3e0f60d183d34a0b0dabd0a
Tianhao <zhangth1990@hotmail.com>
2018-06-18 23:59:29 -0600

    check to see if a user already signed in

--- app/src/main/java/com/example/tianhao/snapchat/MainActivity.kt
+++ app/src/main/java/com/example/tianhao/snapchat/MainActivity.kt
@@ -4,19 +4,29 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.EditText
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
     var emailEditText: EditText? = null
     var passwordEditText: EditText? = null
+    val mAuth = FirebaseAuth.getInstance()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         emailEditText = findViewById(R.id.emailEditText)
         passwordEditText = findViewById(R.id.passwordEditText)
+
+        if (mAuth.currentUser != null) {
+            logIn()
+        }
     }
 
     fun goClicked(view: View){
 
+    }
+    fun logIn(){
+        //Move to next Activity
     }
 }
